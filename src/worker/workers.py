30767a70386aa9c075a23a799e3f52e39fe02590
m3ck0 <m3ck0@pm.me>
2023-11-27 19:46:35 +0400

    [ref] unused var names

--- src/worker/workers.py
+++ src/worker/workers.py
@@ -2,22 +2,22 @@ from rq import Worker
 
 
 class BaseDeathPenalty(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *_, **__):
         pass
 
-    def __enter__(self, *args, **kwargs):
+    def __enter__(self, *_, **__):
         pass
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, *_, **__):
         pass
 
 
 class BasicWindowsWorker(Worker):
     death_penalty_class = BaseDeathPenalty
 
-    def main_work_horse(self, *args, **kwargs):
+    def main_work_horse(self, *_, **__):
         raise NotImplementedError("Test worker does not implement this method")
 
-    def execute_job(self, *args, **kwargs):
+    def execute_job(self, *_, **__):
         """Execute job in same thread/process, do not fork()"""
-        return self.perform_job(*args, **kwargs)
+        return self.perform_job(*_, **__)
