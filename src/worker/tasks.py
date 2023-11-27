0be66390528fa4000c8b1409a1dd797101a9e06d
m3ck0 <m3ck0@pm.me>
2023-11-27 19:44:28 +0400

    [ref]

--- src/worker/tasks.py
+++ src/worker/tasks.py
@@ -30,10 +30,9 @@ def publish_scheduled_posts(caller: str = 'N/A') -> None:
     database = mongo_client.get_database('blog')
     collection = database.get_collection('post')
 
-    current_ts: float = time.time()
     posts_to_be_published = collection.find({
-        'is_published': False
-        # 'publish_at_ts': {'$lte': current_ts}
+        'is_published': False,
+        'publish_at_ts': {'$lte': time.time()}
     })
 
     for post in posts_to_be_published:
@@ -43,23 +42,15 @@ def publish_scheduled_posts(caller: str = 'N/A') -> None:
             response = requests.put(
                 url=f'{API_HOST}/api/posts/{post_id}',
                 json={
-                    "title": "My first post title",
-                    "content": "Lorem Ipsum is simply dummy text",
-                    "publish_at_ts": 0,
-                    "is_published": True
+                    'title': 'My first post title',
+                    'content': 'Lorem Ipsum is simply dummy text',
+                    'publish_at_ts': 0,
+                    'is_published': True
                 }
             )
             response.raise_for_status()
             logger.info(f'post w/ id "{str(post_id)}" is published')
         except Exception:
             logger.error('post update API failed')
-
-
-        # post_id = post.get('_id')
-        # collection.update_one(
-        #     {'_id': post_id},
-        #     {'$set': {'is_published': True}}
-        # )
-        # logger.info(f'post w/ id "{str(post_id)}" is published')
 
     logger.info('"publish_scheduled_posts" task finished execution')
